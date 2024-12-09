53cc62b02b9e61ae87823b00603c64932e3ccdde
Xiangrui Dong <xiangrui@Xiangruis-MacBook-Pro-2.local>
2024-12-08 19:58:55 -0500

    Initial commit

---
+++ src/test/java/org/scraper/githubactionsdemo/GithubActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.scraper.githubactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
